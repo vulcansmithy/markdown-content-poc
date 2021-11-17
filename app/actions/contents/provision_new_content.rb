32954ebd4677fea80b00e42c718a62f0f7d7ca91
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-11-17 23:47:00 +0800

    Define the build action. Create a new Content and pass  the params. Not saving yet. Added awesome_print gem for pretty printing the content of variables.

--- app/actions/contents/provision_new_content.rb
+++ app/actions/contents/provision_new_content.rb
@@ -5,16 +5,25 @@ class ProvisionNewContent < Actionable::Action
   step :validate
   step :create
   
+  def initialize(params)
+    super()
+    @params = params
+  end
+  
   def build
     # @TODO
+puts "@DEBUG L:#{__LINE__}    Running build..."
+     @content = Content.new @params 
   end
   
   def validate
     # @TODO
+puts "@DEBUG L:#{__LINE__}    Running validate..."
   end
 
   def create
     # @TODO
+puts "@DEBUG L:#{__LINE__}    Running create..."
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_17_120556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "markdown_contents", force: :cascade do |t|
+    t.integer "content_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end

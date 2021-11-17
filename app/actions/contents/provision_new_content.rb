0e0fd32afd00d17fe9f5efc5c69011f09181c568
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-11-17 23:09:35 +0800

    Enable ProvisionContent class as actionable. Define steps (empty for now).

--- app/actions/contents/provision_new_content.rb
+++ app/actions/contents/provision_new_content.rb
@@ -1,4 +1,20 @@
 # 1.
-class ProvisionNewContent
+class ProvisionNewContent < Actionable::Action
   
+  step :build
+  step :validate
+  step :create
+  
+  def build
+    # @TODO
+  end
+  
+  def validate
+    # @TODO
+  end
+
+  def create
+    # @TODO
+  end
+
 end
